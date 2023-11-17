051612ca2e966226fbdb5bb3a81669edf5bfebfa
1484-gbs <naoki@gbservice.co.jp>
2023-11-17 16:09:26 +0900

    refactor grid.js

--- src/front/render/hoge.js
+++ src/front/render/hoge.js
@@ -1,5 +1,19 @@
-
+var grid
 document.addEventListener('DOMContentLoaded', async function () {
+    // const header = 'X-XXX-XXX'
+    // const token = 'token'
+    // const data = {}
+
+    // const response = await fetch("/search", {
+    //     method: postMessage,
+    //     body: JSON.stringify(data),
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         [header]: token
+    //     }
+    // })
+    // const result = await response.json()
+
     let select = document.getElementById("position")
     let list = await window.testapi.getPositions()
     list.forEach(p => {
@@ -9,27 +23,35 @@ document.addEventListener('DOMContentLoaded', async function () {
         select.appendChild(option)
     });
 
-    new gridjs.Grid({
-        columns: [{
-            id: "position_id",
-            name: "ID",
-            formatter: (cell) => gridjs.html(`<a href="hoge2.html?id=${cell}">${cell}</a>`),
-        },
-        {
-            id: "position_name",
-            name: "役職名"
-        },
-        {
-            id: "role", name: "ロール"
-        },
-        {
-            id: "display_order", name: "表示順"
-        }],
-        data: list,
-        pagination: {
-            limit: 5
-        }
-    }).render(document.getElementById('wrapper'));
+    if (grid) {
+        if (grid.config.pagenation) grid.config.plugin.remove('pagenation')
+        grid.updateConfig({
+            data: list,
+            pagenation: 5
+        }).forceRender()
+    } else {
+        grid = new gridjs.Grid({
+            columns: [{
+                id: "position_id",
+                name: "ID",
+                formatter: (cell) => gridjs.html(`<a href="hoge2.html?id=${cell}">${cell}</a>`),
+            },
+            {
+                id: "position_name",
+                name: "役職名"
+            },
+            {
+                id: "role", name: "ロール"
+            },
+            {
+                id: "display_order", name: "表示順"
+            }],
+            data: list,
+            pagination: {
+                limit: 5
+            }
+        }).render(document.getElementById('wrapper'));
+    }
 
     const button = document.getElementById('get_btn');
     button.addEventListener('click', async function (clickEvent) {
